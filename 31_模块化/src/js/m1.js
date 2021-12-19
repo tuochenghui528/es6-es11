b2aebbfbdb55f02a6580c15b9706c553a02f0654
tuochenghui <tuochenghui@162.com>
2021-12-19 22:05:59 +0800

    es6-es11

---
+++ 31_模块化/src/js/m1.js
@@ -0,0 +1,6 @@
+//分别暴露
+export const name = '小沈阳';
+
+export function teach() {
+    console.log('我可以逗大家笑');
+}
---
+++ 31_模块化/src/js/m2.js
@@ -0,0 +1,8 @@
+//统一暴露
+const name = '宋小宝';
+
+function play() {
+    console.log('我可以演电视剧');
+}
+
+export {name, play};
